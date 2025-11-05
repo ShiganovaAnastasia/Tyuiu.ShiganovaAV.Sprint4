055a42506bcb0410fb79e800e5b0ed7792085cf2
Anastasia Shiganova <Tyuiu.ShiganovaAV@outlook.com>
2025-11-05 13:23:10 +0500

    .

---
+++ Tyuiu.ShiganovaAV.Sprint4.Task2.V3.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ShiganovaAV.Sprint4.Task2.V3.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint4.Task2.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 8, 2, 7, 5, 0, 7, 4, 7, 5, 7 };
+            int res = ds.Calculate(numsArray);
+            int wait = 14;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShiganovaAV.Sprint4.Task2.V3/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.ShiganovaAV.Sprint4.Task2.V3.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint4.Task2.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 |Выполнила: Шиганова А.В.| ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнила: Шиганова Анастасия Владимировна |ПИНб-25-1|                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int[] numsArray = { 8, 2, 7, 5, 0, 7, 4, 7, 5, 7 };
+            Console.WriteLine("ИСХОДНЫЙ МАССИВ:");
+            for (int i = 0; i < numsArray.Length; i++)
+            {
+                Console.WriteLine(numsArray[i]);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(numsArray);
+            Console.WriteLine(res);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
+
