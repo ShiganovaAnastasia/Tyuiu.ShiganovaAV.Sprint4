2161e45e99fcbad3442f7a91c2f5a2b4a546354a
Anastasia Shiganova <Tyuiu.ShiganovaAV@outlook.com>
2025-11-05 13:06:38 +0500

    .

---
+++ Tyuiu.ShiganovaAV.Sprint4.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ShiganovaAV.Sprint4.Task1.V1.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint4.Task1.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 8, 2, 7, 5, 0, 7, 4, 7, 5, 7 };
+            int res = ds.Calculate(numsArray);
+            int wait = 14;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
